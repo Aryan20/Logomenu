9b712981ab87a3e93a515f6b6b11e13aacf98672
Aryan Kaushik <aryankaushik2023@gmail.com>
2023-11-07 22:53:32 +0530

    logo: add Nobara linux

--- constants.js
+++ constants.js
@@ -27,6 +27,7 @@ export const SymbolicDistroIcons = [
     {PATH: '/Resources/asahilinux-logo-symbolic.svg'},
     {PATH: '/Resources/zorin-logo-symbolic.svg'},
     {PATH: '/Resources/void-logo-symbolic.svg'},
+    {PATH: '/Resources/nobara-logo-symbolic.svg'},
 ];
 
 export const ColouredDistroIcons = [
